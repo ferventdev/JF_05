ea6af4916fa0f1674e81ea7f51bed23ea30e5b76
ferventdev <alex.4everyours@gmail.com>
2017-07-14 14:37:17 +0300

    Task 1 - exit and basic help commands done

--- src/main/java/t01/FileViewer.java
+++ src/main/java/t01/FileViewer.java
@@ -8,11 +8,16 @@ import java.util.Scanner;
  * Created by Aleksandr Shevkunenko on 14.07.2017.
  */
 public class FileViewer {
+    static final String COMMAND_PREVIEW = "You can use the following commands:\n" +
+            "help - to see this prompt again;\n" +
+            "exit - to quit from this program.";
+
     public static void main(String[] args) {
         try (Scanner reader = new Scanner(System.in)) {
+            System.out.println(COMMAND_PREVIEW);
             for (String input = null; ; ) {
                 System.out.print("$ ");
-                input = reader.nextLine();
+                input = reader.nextLine().trim();
                 if (input.equalsIgnoreCase("exit")) break;
                 if (input.isEmpty()) continue;
                 parseCommand(input);
@@ -22,6 +27,16 @@ public class FileViewer {
 
     private static void parseCommand(String input) {
         assert input != null : "The input string mustn't be null!";
-        String[] terms = input.trim().split("\\s+");
+        String[] terms = input.split("\\s+");
+        assert terms.length != 0 : "The input string mustn't be empty!";
+
+        switch (terms[0].toLowerCase()) {
+            case "help":
+                if (input.equalsIgnoreCase("help")) System.out.println(COMMAND_PREVIEW);
+                break;
+            default:
+                System.out.println("Such command doesn't exist, ot its using is incorrect.");
+                System.out.println("Enter - help <COMMAND> - to know how to use the command.");
+        }
     }
 }
